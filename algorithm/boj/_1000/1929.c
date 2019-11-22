a47ed292891e4330db90ccf36f953838cfa63403
gawain6 <gawain6@naver.com>
2019-11-22 22:13:56 +0900

    TIL update

---
+++ algorithm/boj/_1000/1929.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* getPrime(int n);
+
+int main() {
+    int n, m;
+    int *numPtr;
+    scanf("%d %d", &n, &m);
+    
+    
+        
+}
+
+int* getPrime(int n) {
+    if(n<2) return 
+}
---
+++ algorithm/boj/test.py
@@ -0,0 +1,8 @@
+def s(n):
+    if n<1:
+        return []
+    elif n==3:
+        return [2]
+
+if __name__ == "__main__":
+    print(s(-3))
