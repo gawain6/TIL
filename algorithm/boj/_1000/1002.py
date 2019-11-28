dcd0ba7bd7b91f0e6e56e4b238a5c17071f6521f
gawain6 <gawain6@naver.com>
2019-11-28 22:00:05 +0900

    TIL update

--- algorithm/boj/_1000/1002.py
+++ algorithm/boj/_1000/1002.py
@@ -1,17 +1,13 @@
 import sys, math
-import symbol
 
 if __name__ == "__main__":
     # x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
     # x1 = y1 = 0
     # r1 = 13
+    
     # x2 = 40
     # y2 = 0
     # r2 = 37
 
     # cnt = 0
     
-    # x+y = 3, y=? -> y = 3-x
-    x = 5
-    y = 3-x
-    print(y)
