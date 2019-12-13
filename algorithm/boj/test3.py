764bb21ecbcc5adde99c8fcc1121c9e61345d759
gawain6 <gawain6@naver.com>
2019-12-13 21:02:46 +0900

    TIL update

--- algorithm/boj/test3.py
+++ algorithm/boj/test3.py
@@ -18,72 +18,7 @@ class TreeNode:
     @right.setter
     def right(self, right): self.__right=right
 class BST:
-    def __init__(self):
-        self.root=None
-    def get_root(self):
-        return self.root
-    def preorder_traverse(self, cur, func):
-        if not cur: return
-        func(cur)
-        self.preorder_traverse(cur.left, func)
-        self.preorder_traverse(cur.right, func)
-    def insert(self, key):
-        new_node=TreeNode(key)
-        cur=self.root
-        if not cur:
-            self.root=new_node
-            return
-        while True:
-            parent=cur
-            if key<cur.key:
-                cur=cur.left
-                if not cur:
-                    parent.left=new_node
-                    return
-            else:
-                cur=cur.right
-                if not cur:
-                    parent.right=new_node
-                    return
-    def search(self, target):
-        cur=self.root
-        while cur:
-            if cur.key==target:
-                return cur
-            elif cur.key>target:
-                cur=cur.left
-            elif cur.key<target:
-                cur=cur.right
-        return cur
-    def __remove_recursion(self, cur, target):
-        if not cur: return None, None
-        elif target<cur.key:
-            cur.left, rem_node=self.__remove_recursion(cur.left, target)
-        elif target>cur.key:
-            cur.right, rem_node=self.__remove_recursion(cur.right, target)
-        else:
-            if not cur.left and not cur.right:
-                rem_node=cur
-                cur=None
-            elif not cur.right:
-                rem_node=cur
-                cur=cur.left
-            elif not cur.left:
-                rem_node=cur
-                cur=cur.right
-            else:
-                replace=cur.left
-                while replace.right:
-                    replace=replace.right
-                cur.key, replace.key=replace.key, cur.key
-                cur.left, rem_node=self.__remove_recursion(self.left, replace.key)
-        return cur, rem_node
-            
-    def remove(self, target):
-        self.root, removed_node=self.__remove_recursion(self.root, target)
-        if removed_node:
-            removed_node.left=removed_node.right=None
-        return removed_node
+    
 
 if __name__=="__main__":
     print('*'*100)
