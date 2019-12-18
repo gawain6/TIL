e5b4a08330b6a7ad97b19ba94acbd720d7be452d
gawain6 <gawain6@naver.com>
2019-12-18 22:51:23 +0900

    TIL update

--- algorithm/boj/_1000/1018.c
+++ algorithm/boj/_1000/1018.c
@@ -1,79 +1,64 @@
 #include <stdio.h>
 
-// void func1(int * _idx, char * _row, int size, char c) {
-//     if(c=='B') c='W';
-//     else if(c=='W') c='B';
-//     for(int j=1; j<size; j++) {
-//         if(_row[j]==c) {
-//             *_idx=j;
-//             break;
-//         }
-//     }
-// }
-
-// // BBBBWWWW
-
-// int main() {
-//     int idx, cnt=0, n, m;
-//     scanf("%d %d", &n, &m);
-//     char row[m];
-//     for(int i=0; i<n; i++) {
-//         scanf("%s", row);
-        
-//         for(int i=0; i<m; i++) {
-//             if(row[0]=='B') func1(&idx, row, m, 'B');
-//             else if(row[0]=='W') func1(&idx, row, m, 'W');
-//         }
-//         if(idx%2!=0) {
-//             for(int i=0; i<m; i++) {
-//                 if((row[0]=='B')&(i%2!=0)&(row[i]!='W')) cnt++;
-//                 else if((row[0]=='W')&(i%2!=0)&(row[i]!='B')) cnt++;
-//             }
-//         } else {
-//             for(int i=0; i<m; i++) {
-//                 if((row[0]=='B')&(i%2==0)&(row[i]!='W')) cnt++;
-//                 else if((row[0]=='W')&(i%2==0)&(row[i]!='B')) cnt++;
-//             }
-//         }
-//     }
-//     printf("%d\n", cnt);
-//     return 0;
-// }
+char s1[]={"WBWBWBWB"};
+char s2[]={"BWBWBWBW"};
 
 int main() {
-    int i, j, k, cur, idx, n=2, m=13, cnt=0;
-    char c;
-    char row[13]="BBBBBBBBWBWBW";
-    for(i=0; i<=m-8; i++) {
-        cur=0;
-        idx=0;
-        for(j=i; j<i+8; j++) {
-            if(j==i) { // 바꿔야할 문자의 인덱스 홀짝 위치 찾기
-                c=row[j];
-                for(k=i; k<i+8; k++) {
-                    if(c!='W'&row[k]=='W') {
-                        idx=k;
-                        c='W';
-                        break;
-                    } else if(c!='B'&row[k]=='B') {
-                        idx=k;
-                        c='B';
-                        break;
+    int n, m, i, j, k, l, p, q, temp, tempRes=0, res=0;
+    int point[8]={0,};
+    scanf("%d %d", &n, &m);
+    char s[n][m];
+    for(i=0; i<n; i++) {
+        scanf("%s", s[i]);
+    }
+    for(i=0; i<=n-8; i++) { 
+        for(j=0; j<=m-8; j++) {
+            for(k=i; k<i+8; k++) {
+                temp=0;
+                for(l=j; l<j+8; l++) {
+                    int cnt1=0, cnt2=0;
+                    for(p=l, q=0; p<l+8; p++, q++) {
+                        if(s[k][p]!=s1[q]) cnt1++;
+                        if(s[k][p]!=s2[q]) cnt2++;
                     }
+                    if(l!=0) temp=cnt1>cnt2 ? (cnt2>temp ? temp:cnt2):(cnt1>temp ? temp:cnt1);
+                    else if(l==0) temp=cnt1>cnt2 ? cnt2:cnt1;
                 }
+                if(k<8) point[k]=temp;
+                else if(k>=8&point[k%8]>temp) point[k%8]=temp; 
             }
-            if(idx==0) {
-                if(j%2!=0) cur++;
-            } else if(idx%2!=0) {
-                if((j%2!=0)&(row[j]!=c)) cur++; 
-            } else if(idx%2==0) {
-                if((j%2==0)&(row[j]!=c)) cur++;
+            if(i==0&j==0) {
+                for(i=0; i<8; i++) {
+                    res+=point[i];
+                }
+            } else {
+                tempRes=0;
+                for(i=0; i<8; i++) {
+                    tempRes+=point[i];
+                }
+                if(tempRes<res) res=tempRes;
             }
         }
-        if(i==0) cnt=cur;
-        else if(cnt>cur) cnt=cur;
     }
-    printf("%d\n", cnt);
+    // for(i=0; i<n; i++) {
+    //     temp=0;
+    //     for(j=0; j<=m-8; j++) {
+    //         int cnt1=0, cnt2=0;
+    //         for(k=j, l=0; k<j+8; k++, l++) {
+    //             if(s[i][k]!=s1[l]) cnt1++;
+    //             if(s[i][k]!=s2[l]) cnt2++;
+    //         }
+    //         if(j!=0) temp=cnt1>cnt2 ? (cnt2>temp ? temp:cnt2):(cnt1>temp ? temp:cnt1);
+    //         else if(j==0) temp=cnt1>cnt2 ? cnt2:cnt1;
+    //     }
+    //     if(i<8) point[i]=temp;
+    //     else if(i>=8&point[i%8]>temp) point[i%8]=temp; 
+    // }
+    // for(i=0; i<8; i++) {
+    //     res+=point[i];
+    // }
+    printf("%d\n", res);
+    return 0;
 }
 
 // BBBBB BWBWBWBW 1
