2ca8f5b3c6ef1e6d5f71d0454730fb8cf0cfe1ee
gawain6 <gawain6@naver.com>
2020-07-25 07:49:22 +0900

    TIL update

--- algorithm/boj/_1000/1330.cc
+++ algorithm/boj/_1000/1330.cc
@@ -1,10 +1,17 @@
 #include <iostream>
-
 using namespace std;
-int main() {
-    int n1, n2;
-    cin >> n1 >> n2;
-    if(n1>n2) cout << ">" << endl;
-    else if(n1<n2) cout << "<" << endl;
+
+/*
+ * 1. A, B를 입력 받는다.
+ * 2. A가 클 경우 '>' 출력, 반대는 '<', 같으면 '==' 출력
+ */
+
+int main(void) {
+    int A, B;
+
+    cin >> A >> B;
+    if(A>B) cout << ">" << endl;
+    else if(A<B) cout << "<" << endl;
     else cout << "==" << endl;
+    return 0;
 }
