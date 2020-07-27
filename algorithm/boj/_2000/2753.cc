d35d265218db07f63fbe715fba539e93795fd904
gawain6 <gawain6@naver.com>
2020-07-27 14:36:15 +0900

    TIL update

--- algorithm/boj/_2000/2753.cc
+++ algorithm/boj/_2000/2753.cc
@@ -1,9 +1,18 @@
 #include <iostream>
-
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    if(n%400==0|(n%4==0&n%100!=0)) cout << 1 << endl;
-    else cout << 0 << endl;
+
+/*
+ * 1. 연도를 입력 받는다.
+ * 2. 윤년을 if문의 조건으로 확인한다.
+ * 3. 윤년의 조건은 4의 배수이면서 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+ * 4. 윤년이면 1 아니면 0을 출력한다.
+ */
+
+int main(void) {
+    int year;
+    
+    cin >> year;
+    if(year%4 == 0 && (year%100 != 0 || year%400 == 0)) cout << "1\n";
+    else cout << "0\n";
+    return 0;
 }
