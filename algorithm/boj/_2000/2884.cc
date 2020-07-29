44b881d86d426f9c63f75cb3fdf26c87f2dee363
gawain6 <gawain6@naver.com>
2020-07-29 15:58:27 +0900

    TIL update

--- algorithm/boj/_2000/2884.cc
+++ algorithm/boj/_2000/2884.cc
@@ -1,14 +1,23 @@
 #include <iostream>
-
 using namespace std;
-int main() {
-    int h, m;
-    cin >> h >> m;
-    if(m>=45) m = m-45;
-    else {
-        if(h-1==-1) h=23;
-        else h -= 1;
-        m += 15;
+
+/*
+ * 1. 한 줄에 시간(H)과 분(M)을 입력받는다.
+ * 2. M -= 45를 한다.
+ * 3. M<=0 이면 H, M을 그대로 출력한다.
+ * 3. 음수가 나올 경우 H-1을 하고, M = 60 - M 을 하고, H, M을 출력한다.
+ * 4. 0시 이전으로 알람이 맞춰지면 23시로 출력한다.
+ */
+
+int main(void) {
+    int H, M;
+    
+    cin >> H >> M;
+    M -= 45;
+    if(M < 0) {
+        H = (H-1 < 0) ? 23 : H-1;
+        M = 60 + M; 
     }
-    cout << h << " " << m << endl;
+    cout << H << " " << M << endl;
+    return 0;
 }
