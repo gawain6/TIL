d30bfea16a63abb9e06718e3a99fece7ddc46b28
gawain6 <gawain6@naver.com>
2020-08-01 22:57:48 +0900

    TIL update

--- algorithm/boj/_8000/8393.cpp
+++ algorithm/boj/_8000/8393.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n, ret=0;
+/*
+ * 1. n을 입력받는다.
+ * 2. for문으로 1부터 n까지의 합을 더해 출력한다.
+ */
+
+int main(void) {
+    int n, res = 0;
+
     cin >> n;
     for(int i=1; i<=n; i++) {
-        ret+=i;
+        res += i;
     }
-    cout << ret << endl;
+    cout << res << endl;
+    return 0;
 }
