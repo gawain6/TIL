5b3b3f212d5b3a0bcdf3075842bb1ba5cb03ebff
gawain6 <gawain6@naver.com>
2020-08-02 19:18:48 +0900

    TIL update

--- algorithm/boj/_15000/15552.cpp
+++ algorithm/boj/_15000/15552.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
-int main() {
-    int t, a, b;
-    scanf("%d", &t);
-    for(int i=0; i<t; i++) {
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a+b);
+/*
+ * 1. 첫 번째 줄에는 테스트 케이스 개수 T를 입력받는다.
+ * 2. for문으로 T만큼 정수 A, B를 입력받고, A+B의 합을 출력한다. 
+ */
+
+int main(void) {
+    int T, A, B;
+
+    scanf("%d", &T);
+    for(int i=0; i<T; i++) {
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A+B);
     }
+    return 0;
 }
