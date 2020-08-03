682019c04a3c62d0267455d245b8f1356dd2c39e
gawain6 <gawain6@naver.com>
2020-08-03 21:56:50 +0900

    TIL update

--- algorithm/boj/_2000/2741.cpp
+++ algorithm/boj/_2000/2741.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-using namespace std;
 
-int main() {
+/*
+ * 1. n을 입력받는다.
+ * 2. for문으로 1부터 n까지 한 줄씩 숫자를 출력한다.
+ */
+
+int main(void) {
     int n;
-    cin >> n;
+
+    scanf("%d", &n);
     for(int i=1; i<=n; i++) {
-        cout << i << "\n";
+        printf("%d\n", i);
     }
+    return 0;
 }
