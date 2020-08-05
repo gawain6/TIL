c7206b5a06131cd5bbec602dcaa829d8b3d6b7a0
gawain6 <gawain6@naver.com>
2020-08-05 21:41:39 +0900

    TIL update

--- algorithm/boj/_11000/11021.cpp
+++ algorithm/boj/_11000/11021.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    int t, a, b;
-    cin >> t;
-    for(int i=1; i<=t; i++) {
-        cin >> a >> b;
-        cout << "Case #" << i << ": " << a+b << '\n';
+/*
+ * 1. 첫 줄에 테스트 케이스 T를 입력받는다.
+ * 2. for문으로 T만큼 A, B를 입력받고, A+B값을 Case #1: 2 형식으로 출력한다.
+ */
+
+int main(void) {
+    int T, A, B;
+
+    scanf("%d", &T);
+    for(int i=1; i<=T; i++) {
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d\n", i, A+B);
     }
+    return 0;
 }
