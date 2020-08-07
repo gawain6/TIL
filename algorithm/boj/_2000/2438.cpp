2691b02ba243c547012ddbb76058d2ea9c768cc6
gawain6 <gawain6@naver.com>
2020-08-07 13:50:02 +0900

    TIL update

--- algorithm/boj/_2000/2438.cpp
+++ algorithm/boj/_2000/2438.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<=i; j++) {
-            cout << "*";
+/*
+ * 1. 첫 줄에 정수 N을 입력받는다.
+ * 2. for문으로 N개의 줄 만큼 *를 출력한다. 단, N번 째 줄에는 N개의 *을 출력한다.
+ * 3-1. 2중 for문으로 구성한다.
+ * 3-2. 바깥 쪽 for문은 i=1 부터 N까지 줄을 카운터한다.
+ * 3-3. 안 쪽 for문은 j=0부터 시작해서 i값과 같아지기 전까지 한 줄에 *를 반복 출력한다.
+ * 3-4. j==i 가 되면 안 쪽 for문을 종료하고, 개행문자를 출력한다.
+ */
+
+int main(void) {
+    int N, i, j;
+
+    scanf("%d", &N);
+    for(i=1; i<=N; i++) {
+        for(j=0; j<i; j++) {
+            printf("*");
         }
-        cout << '\n';
+        printf("\n");
     }
+    return 0;
 }
