59d1f7ebe979838dce4d6cd1811dc0ad5719b7c2
gawain6 <gawain6@naver.com>
2020-08-08 14:54:09 +0900

    TIL update

--- algorithm/boj/_2000/2439.cpp
+++ algorithm/boj/_2000/2439.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        for(int j=n-1; j>i; j--) {
-            cout << " ";
+/*
+ * 1. *을 몇 줄을 출력할 것인지를 첫 줄에서 N을 입력받는다.
+ * 2. for문을 이용해서 *을 우측정렬로 출력한다.
+ * 3. 바깥쪽 for문은 i값이 1 부터 N까지 반복한다.
+ * 4. 안쪽 for문1은 j값이 0 부터 N-i까지 한 줄에 공백 출력을 반복한다.
+ * 5. 안쪽 for문2는 l값이 j 부터 N과 같기 전까지 * 출력을 반복한다.
+ * 6. 안쪽 for문1을 빠져나오면 개행문자를 출력한다.
+ */
+
+int main(void) {
+    int N, i, j, l;
+
+    scanf("%d", &N);
+    for(i=1; i<=N; i++) {
+        for(j=0; j<N-i; j++) {
+            printf(" ");
         }
-        for(int k=0; k<=i; k++) {
-            cout << "*";
+        for(l=j; l<N; l++) {
+            printf("*");
         }
-        cout << '\n';
+        printf("\n");
     }
+    return 0;
 }
