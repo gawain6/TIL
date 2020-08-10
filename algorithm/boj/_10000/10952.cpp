199f8518767953e4339fe87971d61f8516370a39
gawain6 <gawain6@naver.com>
2020-08-10 15:53:14 +0900

    TIL update

--- algorithm/boj/_10000/10952.cpp
+++ algorithm/boj/_10000/10952.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    int a, b;
-    while(true) {
-        cin >> a >> b;
-        if(a==0 & b==0) break;
-        cout << a+b << '\n';
+/*
+ * 1. A, B를 입력받고, A+B의 합을 출력을 0, 0이 입력되기 전까지 반복한다.
+ * 2. 반복은 while문을 사용한다.
+ */
+
+int main(void) {
+    int A, B;
+    while(1) {
+        scanf("%d %d", &A, &B);
+        if(A==0 && B==0) break;
+        printf("%d\n", A+B);
     }
+    return 0;
 }
