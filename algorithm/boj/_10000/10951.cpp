9e0e11e868e77da99444fbb0a7885926a79400a7
gawain6 <gawain6@naver.com>
2020-08-11 17:58:54 +0900

    TIL update

--- algorithm/boj/_10000/10951.cpp
+++ algorithm/boj/_10000/10951.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a, b;
-    while(cin >> a >> b) {
-        cout << a+b << '\n';
+/*
+ * 1. A, B를 입력 받는다.
+ * 2. A+B를 출력한다.
+ * 3. 위를 while로 반복한다.
+ * 4. EOF면 반복을 종료한다.
+ */
+
+int main(void) {
+    int A, B;
+    while(1) {
+        cin >> A >> B;
+        if(cin.eof()) break;
+        cout << A+B << endl;
     }
+    return 0;
 }
