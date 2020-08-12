9b2fa408a445988f0c7b01b2d127da22b67a3646
gawain6 <gawain6@naver.com>
2020-08-12 19:43:35 +0900

    TIL update

--- Lecture/200811/200811/200811.cpp
+++ Lecture/200811/200811/200811.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+namespace A
+{
+	void Render()
+	{
+		cout << 'A' << endl;
+	}
+}
+
+namespace B
+{
+	void Render()
+	{
+		cout << 'B' << endl;
+	}
+}
+
 void main()
 {
+	A::Render();
+	B::Render();
 	cout << "Hello World!" << endl;
 }
--- algorithm/boj/_1000/1110.cpp
+++ algorithm/boj/_1000/1110.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    int n, a, b, ret, cnt=0;
-    cin >> n;
-    ret = n;
-    do {
-        a = ret%10;
-        b = ((ret%10)+(ret>=10 ? ret/10 : 0))%10;
-        ret = a*10+b;
-        cnt++;
-    } while(to_string(n)!=to_string(ret));
-    cout << cnt << '\n';
+/*
+ * 1. 임의의 숫자 N을 입력받는다.
+ * 2. N의 1의 자리수와 10의 자리수를 더한다.
+ * 3. N의 1의 자리수를 10의 자리수로, 더해서 나온 값의 1의 자리수를 1의 자리수로 해서 새로운 숫자를 만든다.
+ * 4. 새로 만든 값이 N과 같은지 확인하고, 같지 않으면 count한다.
+ * 5. N과 같은 값이 나올 때 까지 while문으로 2~4를 반복한다.
+ * 6. N과 같은 값이 나오면 count한 값을 출력한다.
+ */
+
+int main(void) {
+    int N, count=1, sum;
+    scanf("%d", &N);
+    sum = N;
+    while(1) {
+        sum = (sum%10)*10 + ((sum%10) + (sum/10))%10;
+        if(sum == N) break;
+        count++;
+    }
+    printf("%d", count);
+    return 0;
 }
