0bd75270e1645558772b1252e6faaf1ee8939f6a
gawain6 <gawain6@naver.com>
2020-08-14 15:35:50 +0900

    TIL update

--- Lecture/200813_homework/200813_homework/HomeWork.cpp
+++ Lecture/200813_homework/200813_homework/HomeWork.cpp
@@ -5,12 +5,13 @@ using namespace std;
 1. int형 변수 korean, english, math를 선언하고, 한 줄씩 점수를 입력받는다.
 2. 수학을 마지막으로 입력받으면 ############## 을 출력한다.
 3. 국어, 영어, 수학 순으로 한 줄씩 점수를 출력한다.
-4. 마지막 줄에 ############## 을 출력한다.
+4. 총점과 평균을 한 줄씩 출력한다.
+5. 마지막 줄에 ############## 을 출력한다.
 */
 
 void main()
 {
-	int korean, english, math;
+	int korean, english, math, total, avg;
 
 	cout << "국어: ";
 	cin >> korean;
@@ -18,10 +19,14 @@ void main()
 	cin >> english;
 	cout << "수학: ";
 	cin >> math;
+	total = korean + english + math;
+	avg = total / 3;
 
 	cout << "################" << endl;
 	cout << "국어 점수: " << korean << endl;
 	cout << "영어 점수: " << english << endl;
 	cout << "수학 점수: " << math << endl;
+	cout << "총점: " << total << endl;
+	cout << "평균: " << avg << endl;
 	cout << "################" << endl;
 }
