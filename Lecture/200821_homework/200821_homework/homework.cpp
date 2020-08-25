dd590e2bdcc714139199948b468f5e7dfb0dd6a7
gawain6 <gawain6@naver.com>
2020-08-25 13:00:13 +0900

    TIL update

--- Lecture/200821_homework/200821_homework/homework.cpp
+++ Lecture/200821_homework/200821_homework/homework.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void main() {
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j <= i; ++j) {
+void main()
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
 			cout << '*';
 		}
 		cout << endl;
@@ -11,8 +14,10 @@ void main() {
 
 	cout << "\n=========================\n" << endl;
 
-	for (int i = 5; i > 0; --i) {
-		for (int j = 1; j <= i; ++j) {
+	for (int i = 5; i > 0; --i)
+	{
+		for (int j = 1; j <= i; ++j)
+		{
 			cout << '*';
 		}
 		cout << endl;
@@ -20,11 +25,14 @@ void main() {
 
 	cout << "\n=========================\n" << endl;
 
-	for (int i = 0; i < 5; ++i) {
-		for (int j = i; j < 4; ++j) {
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = i; j < 4; ++j)
+		{
 			cout << ' ';
 		}
-		for (int j = 0; j <= i; ++j) {
+		for (int j = 0; j <= i; ++j)
+		{
 			cout << '*';
 		}
 		cout << endl;
@@ -32,11 +40,14 @@ void main() {
 
 	cout << "\n=========================\n" << endl;
 
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < i; ++j) {
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
 			cout << ' ';
 		}
-		for (int j = i; j < 5; ++j) {
+		for (int j = i; j < 5; ++j)
+		{
 			cout << '*';
 		}
 		cout << endl;
--- algorithm/boj/_10000/10818.cpp
+++ algorithm/boj/_10000/10818.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n, temp, min, max;
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        cin >> temp;
-        if(i==0) min=max=temp;
-        if(min>temp) min=temp;
-        if(max<temp) max=temp;
-    }
-    cout << min << " " << max << '\n';
+int main()
+{
+	int N, temp, max = 0, min = 0;
+	cin >> N;
+
+	for(int i = 0; i < N; ++i)
+	{
+		cin >> temp;
+		if(0 == i) max = min = temp;
+		else if(temp > max) max = temp;
+		else if(temp < min) min = temp;
+	}
+
+	cout << min << " " << max << endl;
 }
