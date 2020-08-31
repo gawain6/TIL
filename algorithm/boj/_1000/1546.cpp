87a8f32fad21217ae8da4034992c64c04459b312
gawain6 <gawain6@naver.com>
2020-08-31 17:06:13 +0900

    TIL update

--- algorithm/boj/_1000/1546.cpp
+++ algorithm/boj/_1000/1546.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * 1. 첫 줄에 시험 과목 N을 입력받는다. (N <= 1000)
+ * 2. 둘 째 줄에 N만큼 값을 입력받는다. (0 <= x <= 100)
+ * 3. 성적 값 중 최댓값을 M에 넣고, 모든 점수를 점수/M*100 으로 고친다.
+ * 4. 마지막으로 새로운 평균을 출력한다.
+ */
+
 int main() {
-    int n, i;
-    double m=0, sum=0;
+    int n;
+    float max = 0, total = 0;
+    float ar[1000] = {};
+
     cin >> n;
-    double point[n];
-    for(i=0; i<n; i++) {
-        cin >> point[i];
-        if(point[i]>m) m=point[i];
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> ar[i];
+        if(ar[i] > max) max = ar[i];
     }
-    for(i=0; i<n; i++) {
-        point[i]=point[i]/m*100;
-        sum+=point[i];
+    for(int i = 0; i < n; ++i)
+    {
+        ar[i] = ar[i]/max*100.f;
+        total += ar[i];
     }
-    printf("%.6f\n", sum/n);
+    cout << total / (float)n << endl;
 }
--- algorithm/boj/_3000/3052.cpp
+++ algorithm/boj/_3000/3052.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * 1. 숫자 10개를 각 줄에 입력받는다. (0 <= x <= 1000)
+ * 2. 각 숫자를 42로 나눈 나머지를 구한뒤 서로 다른 값이 몇 개인지 출력한다.
+ * 3. ar[99] 에 나머지 값을 idx로 해서 ++ar[값] 연산을 한다.
+ * 4. for문으로 0이 아닌 ar 요소를 count 하여 그 값을 출력한다.
+ */
+
 int main() {
-    int n, i, j, cnt=1;
-    int arr[10];
-    for(i=0; i<10; i++) {
-        cin >> n;
-        arr[i]=n%42;
+    int N, cnt = 0;
+    int ar[99] = {};
+
+    for(int i = 0; i < 10; ++i)
+    {
+        cin >> N;
+        ++ar[N%42];
     }
-    for(i=0; i<9; i++) {
-        for(j=i+1; j<10; j++) if(arr[i]==arr[j]) break;
-        if(arr[i]!=arr[j]) cnt++;
+    for(int i = 0; i < sizeof(ar) / sizeof(int); ++i)
+    {
+        if(0 != ar[i])
+            ++cnt;
     }
-    cout << cnt << '\n';
+    cout << cnt << endl;
 }
