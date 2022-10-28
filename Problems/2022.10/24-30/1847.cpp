fa02071441cd9e9de50b27b6b3055c794c9974b8
김우형 <gawain6@naver.com>
2022-10-29 00:38:16 +0900

    til

--- Problems/2022.10/24-30/1847.cpp
+++ Problems/2022.10/24-30/1847.cpp
@@ -8,10 +8,43 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-    cin >> n;
+    stack<int> s;
 
-    
+    int n, k;
+    int num = 0;
+
+    string str = "";
+
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> k; // 3
+
+        if (s.empty())
+        {
+            s.push(++num);
+            str.append("+\n");
+        }
+
+        while (k != s.top() && k > num)
+        {
+            s.push(++num);
+            str.append("+\n");
+        }
+        
+        if (k == s.top())
+        {
+            s.pop();
+            str.append("-\n");
+        }
+        else
+        {
+            str = "NO";
+            break;
+        }
+    }
+
+    cout << str;
 
     return(0);
 }
