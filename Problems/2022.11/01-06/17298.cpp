5db56820bbccea72f6f8025db769f4146dd88519
김우형 <gawain6@naver.com>
2022-11-03 02:48:23 +0900

    til

---
+++ Problems/2022.11/01-06/17298.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int a[1000000];
+int ans[1000000];
+stack<int> s;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = n - 1; i >=0; --i)
+    {
+        while (!s.empty() && s.top() <= a[i]) s.pop();
+        if (s.empty()) ans[i] = -1;
+        else ans[i] = s.top();
+        s.push(a[i]);
+    }
+    for (int i = 0; i < n; ++i) cout << ans[i] << ' ';
+
+	return 0;
+}
